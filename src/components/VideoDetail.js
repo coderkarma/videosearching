8713150b2b31cb50128a230e92d1ab4060957ac7
karma <karmadrukpa11@gmail.com>
2019-06-19 14:15:54 -0700

    added spinner for loading and fixed grid

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,20 +3,19 @@ import React from 'react';
 const VideoDetail = ({ video }) => {
 	//  added check to add loading before video gets loaded
 	if (!video) {
-		 return <div>Loading...</div>;
-		// return (
-		// 	<div className='text-center'>
-		// 		<div className='spinner-border' role='status'>
-		// 			<span className='sr-only'>Loading...</span>
-		// 		</div>
-		// 	</div>
-		// );
+		return (
+			<div className='text-center'>
+				<div className='spinner-border' role='status'>
+					<span className='sr-only'>Loading...</span>
+				</div>
+			</div>
+		);
 	}
 	const videoId = video.id.videoId;
 	const url = `https://www.youtube.com/embed/${videoId}`;
 
 	return (
-		<div className='video-detail col-md-8 '>
+		<div className='video-detail'>
 			<div className='embed-responsive embed-responsive-16by9'>
 				<iframe
 					className='embed-responsive-item'
